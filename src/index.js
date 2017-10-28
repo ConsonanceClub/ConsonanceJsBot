9f592fae5d3a8de2db4e713f804d9372a03f5f3f
Perfect Makanju <damiperfect@gmail.com>
2017-10-28 04:41:57 +0100

    Refactor codebase structure to allow for tests
    - Create a dummy index.test.js
    - Add .gitgnore
    - Fix running botkit on start

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const Botkit = require('botkit')
+
+const token = process.env.SLACK_TOKEN
+
+const controller = Botkit.slackbot({
+  // reconnect to Slack when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+const bot = controller.spawn({
+    token
+})
+
+bot.startRTM((err, bot, payload) => {
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+})
+
+controller.on('bot_channel_join', (bot, message) => {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears(['hi'], ['ambient', 'direct_message','direct_mention','mention'], (bot, message) => {
+  bot.reply(message, 'Hello.')
+})
